db764aabaad0586c3f54dd0dca4937ca1134b02c
Atharva Umbarkar <57295061+AtharvaUmbarkar@users.noreply.github.com>
2023-04-09 16:54:07 +0530

    Final CORS Fix (hopefully)

--- index.js
+++ index.js
@@ -13,6 +13,14 @@ mongoose.connect(mongoPort);
 console.log(`MongoDB server started`);
 
 const app = express();
+
+app.use(
+  cors({
+    origin: ["*", "http://localhost:3000", "http://127.0.0.1:3000"],
+    methods: ["GET", "POST", "DELETE", "UPDATE", "PUT", "PATCH"],
+  })
+);
+
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ limit: "50mb", extended: true }));
 
@@ -21,19 +29,6 @@ app.use("/api/user/", require("./routes/user.js"));
 app.get("/", async (req, res) => {
   res.send("Hello, Authenticator backend here -_-");
 });
-
-// app.use(function(req, res, next) {
-//   res.header("Access-Control-Allow-Origin", "*");
-//   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-//   next();
-// });
-
-app.use(
-  cors({
-    origin: ["*", "http://localhost:3000"],
-    methods: ["GET", "POST", "DELETE", "UPDATE", "PUT", "PATCH"],
-  })
-);
 
 app.listen(port, () => {
   console.log(`[server]: Server is running at http://localhost:${port}`);
